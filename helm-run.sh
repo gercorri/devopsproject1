30fd6b4344339879f1f17f5cba60d4e5538d4de2
Gerard Corrigan <gercorri@gmail.com>
2019-03-30 01:21:16 +0530

    Update helm-run.sh

--- helm-run.sh
+++ helm-run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 sudo chmod 777 /var/run/docker.sock
 $(aws ecr get-login --no-include-email --region us-east-1)
+sleep 5
 #kubectl create serviceaccount --namespace kube-system tiller
 #kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 #kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-helm init
+/usr/local/bin/helm init
 sleep 10
-helm install devapp --name dev
+/usr/local/bin/helm install devapp --name dev
+sleep  5
+/usr/local/bin/helm ls
 
